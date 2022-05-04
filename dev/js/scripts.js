015bb7a2e04fbdec5bd9f9b03dd39e225ce154b4
fionatdesign <ft98@drexel.edu>
2022-05-03 20:34:25 -0400

    add gsock animation and updated html title

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,5 +4,17 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
+MorphSVGPlugin.convertToPath("#Circle");
 
-const mainTL = gsap.timeline();
+const mainTL = gsap.timeline({id: "mainTL"});
+
+// mainTL.to("#Circle", {duration:3, x:50, ease:"bounce.out"})
+
+// ;
+
+mainTL.from("#Circle", {duration:1, drawSVG: "50% 50%"})
+.from("#Circle", {duration:1, fill:"#fff"})
+// .to("#Circle", {duration:1, fill:"#ff000"})
+.to("#Circle", {duration:1, morphSVG:"#Triangle", fill:"#green"})
+
+;
